78b7950b48c9ea5ffcf9ee0ab77c8cd676d6b616
sudheendra31 <57806261+sudheendra31@users.noreply.github.com>
2021-10-31 00:21:12 +0530

    initial version
    initial version

---
+++ main.py
@@ -0,0 +1,8 @@
+# conda activate aiml
+import nltk
+text = "Backgammon is one of the oldest known board games. Its history can be traced back nearly 5,000 years to archeological discoveries in the Middle East. It is a two player game where each player has fifteen checkers which move between twenty-four points according to the roll of two dice."
+sentences = nltk.sent_tokenize(text)
+for sentence in sentences:
+    print(sentence)
+    words = nltk.word_tokenize(sentence)
+    print(words)
